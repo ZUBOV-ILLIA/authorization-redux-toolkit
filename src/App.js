f7618ffc532b65a4b82c6d3eb73b0633f8d5eabb
ZUBOV ILLIA <zubovillia@gmail.com>
2022-09-25 10:55:18 +0300

    added routes and new pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import { HomePage } from 'Pages/HomePage';
+import { RegisterPage } from 'Pages/RegisterPage';
+import { LoginPage } from 'Pages/LoginPage';
 
 function App() {
   return (
-    <>
-      cleared page
-    </>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/login' component={LoginPage} />
+      <Route exact path='/register' component={RegisterPage} />
+    </Switch>
   );
 }
 
